5da8ef6ead8b12f48b349ef3c9a6e624ef119a5e
ldimartin0 <luke.dnu@gmail.com>
2021-11-30 15:12:48 -0500

    Package fully developed: cleaned function, added namespaces, and added imports

--- R/clean_data.R
+++ R/clean_data.R
@@ -1,61 +1,68 @@
-library(tidyverse)
+library(dplyr)
 library(lubridate)
 library(purrr)
 library(stringr)
 library(forcats)
 
 
-# clean_data is a pipeline function that calls four functions built 
-## inside of the clean_data function.
-## Those functions are clean_nulls, clean_stop_info, 
-## clean_search_vars, and clean_other_vars
-
-# The obvious implementation is `d <- get_all_data() %>% clean_data()
-
+#' Clean MPD Data
+#'
+#' This function takes raw dataframes imported from MPD's website with metropdcleanR and cleans them for analysis.
+#'
+#' @include get_data.R
+#' @param d A dataframe imported with metropdcleanR from MPD's website. Note: This function will not work on dataframes not in their original form from MPD.
+#'
+#' @return A cleaned dataframe.
+#' @export
+#'
+#' @examples
+#'
+#' d <- metropdcleanR::get_yr_data(2019)
+#' d <- clean_data(d)
 clean_data <- function(d) {
-	
+
 	clean_nulls <- function(d) {
 		d[d == "NULL" | is.null(d) | d == "NULL ;"] <- NA
 		d[is.null(d)] <- NA
-		
+
 		return(d)
 	}
-	
+
 	clean_stop_info <- function(d) {
-		
+
 		#stop_type
-		d$stop_type <- recode(d$stop_type,
+		d$stop_type <- dplyr::recode(d$stop_type,
 													"Harbor" = "Harbor",
 													"Non-ticket Stop" = "No Ticket",
 													"Ticket Only" = "Ticket",
 													"Ticket and Non-ticket Stop" = "Both"
 		)
-		
+
 		#stop_date
-		d <- mutate(d, stop_date = mdy(stop_date))
-		
+		d <- dplyr::mutate(d, stop_date = lubridate::mdy(stop_date))
+
 		#stop_time
-		d <- d %>% 
-			separate(col = stop_time, into = c("time_unformatted", "am_pm"), sep = " ", fill = "right") %>% 
-			separate(col = time_unformatted, into = c("stop_time_hrs", "stop_time_mins"), sep = ":", convert = T) %>% 
-			mutate(stop_time_hrs = ifelse(am_pm == "PM", stop_time_hrs+12, stop_time_hrs)) %>%
-			select(-am_pm)
-		
+		d <- d %>%
+			tidyr::separate(col = stop_time, into = c("time_unformatted", "am_pm"), sep = " ", fill = "right") %>%
+			tidyr::separate(col = time_unformatted, into = c("stop_time_hrs", "stop_time_mins"), sep = ":", convert = T) %>%
+			dplyr::mutate(stop_time_hrs = ifelse(am_pm == "PM", stop_time_hrs+12, stop_time_hrs)) %>%
+			dplyr::select(-am_pm)
+
 		#stop_location_block
-		d <- mutate(d, stop_quadrant = str_squish(str_extract(stop_location_block, " NW | NE | SW | SE ")))
-		
+		d <- dplyr::mutate(d, stop_quadrant = stringr::str_squish(stringr::str_extract(stop_location_block, " NW | NE | SW | SE ")))
+
 		#stop_district
-		d <- mutate(d, stop_district = as.integer(str_remove_all(stop_district, "[^0-9]")))
-		
+		d <- dplyr::mutate(d, stop_district = as.integer(stringr::str_remove_all(stop_district, "[^0-9]")))
+
 		#stop_duration_mins
-		d <- mutate(d, stop_duration_minutes = as.integer(str_remove_all(stop_duration_minutes, "[^0-9]")))
-		
+		d <- dplyr::mutate(d, stop_duration_minutes = as.integer(stringr::str_remove_all(stop_duration_minutes, "[^0-9]")))
+
 		#stop_reason_nonticket
-		d <- mutate(d, stop_reason_nonticket = str_remove_all(stop_reason_nonticket, "[^[:punct:]|[:alnum:]|[ ]]"))
-		
+		d <- dplyr::mutate(d, stop_reason_nonticket = stringr::str_remove_all(stop_reason_nonticket, "[^[:punct:]|[:alnum:]|[ ]]"))
+
 		return(d)
 	}
-	
+
 	clean_search_vars <- function(d) {
 		cols_to_logical <- c(
 			"person_search_or_protective_pat_down",
@@ -70,48 +77,47 @@ clean_data <- function(d) {
 			"property_search_warrant",
 			"property_search_consent"
 			)
-		
-		d <- d %>% mutate(across(.cols = all_of(cols_to_logical), .fns = as.logical))
-		
+
+		d <- d %>% dplyr::mutate(dplyr::across(.cols = dplyr::all_of(cols_to_logical), .fns = as.logical))
+
 		cols_small_cleaning <- c(
-			"person_search_reason_consent",                
-			"person_search_reason_probable_cause",         
-			"person_protective_pat_down_reason",           
-			"person_search_reason_warrant",                
-			"property_search_reason_consent",              
-			"property_search_reason_probable_cause",       
-			"property_protective_pat_down_reason",         
-			"property_search_reason_warrant",              
-			"property_search_object_consent",              
-			"property_search_object_probable_cause",       
-			"property_protective_pat_down_object",         
-			"property_search_object_warrant",              
-			"person_search_object_seized_consent",         
-			"person_search_object_seized_probable_cause",  
-			"person_protective_pat_down_object_seized",    
-			"person_search_object_seized_warrant",         
-			"property_search_object_seized_consent",       
+			"person_search_reason_consent",
+			"person_search_reason_probable_cause",
+			"person_protective_pat_down_reason",
+			"person_search_reason_warrant",
+			"property_search_reason_consent",
+			"property_search_reason_probable_cause",
+			"property_protective_pat_down_reason",
+			"property_search_reason_warrant",
+			"property_search_object_consent",
+			"property_search_object_probable_cause",
+			"property_protective_pat_down_object",
+			"property_search_object_warrant",
+			"person_search_object_seized_consent",
+			"person_search_object_seized_probable_cause",
+			"person_protective_pat_down_object_seized",
+			"person_search_object_seized_warrant",
+			"property_search_object_seized_consent",
 			"property_search_object_seized_probable_cause",
-			"property_protective_pat_down_object_seized",  
+			"property_protective_pat_down_object_seized",
 			"property_search_object_seized_warrant"
 		)
-		
-		d <- mutate(d, across(.cols = all_of(cols_small_cleaning), .fns = ~str_remove_all(., "[^[:punct:]|[:alnum:]|[ ]]")))
-		
+
+		d <- dplyr::mutate(d, dplyr::across(.cols = dplyr::all_of(cols_small_cleaning), .fns = ~stringr::str_remove_all(., "[^[:punct:]|[:alnum:]|[ ]]")))
+
 		return(d)
 	}
-	
+
 	clean_other_vars <- function(d) {
-		d <- mutate(d, across(.cols = contains("count"), .fns = as.integer))
-		
+		d <- dplyr::mutate(d, dplyr::across(.cols = dplyr::contains("count"), .fns = as.integer))
+
 		return(d)
 	}
-	
-	d <- compose(clean_nulls, clean_stop_info, clean_search_vars, clean_other_vars, .dir = "forward")(d)
-	
+
+	d <- purrr::compose(clean_nulls, clean_stop_info, clean_search_vars, clean_other_vars, .dir = "forward")(d)
+
 	return(d)
 }
 
 
 
-	
---
+++ R/get_all_data.R
@@ -0,0 +1,10 @@
+library(readr)
+library(dplyr)
+library(purrr)
+library(magrittr)
+
+get_all_data <- function(export = F) {
+	d <- metropdcleanR::get_yr_data(year = 2019:2021, export = export)
+
+	return(d)
+}
